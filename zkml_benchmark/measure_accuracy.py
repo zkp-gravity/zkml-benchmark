34b54e0955938d0e0e185008227eaf4271fbd73f
Georg Wiese <georgwiese@gmail.com>
2023-06-13 14:03:24 +0000

    Update model, refactor

--- zkml_benchmark/measure_accuracy.py
+++ zkml_benchmark/measure_accuracy.py
@@ -3,18 +3,20 @@ import os
 import pickle
 import tempfile
 
-import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from run_args import run_args
-from torch import nn, optim
+# Needed to load pickle file
 from train_mnist import LeNet
 
 import ezkl
 
 
-def write_input_file(x, model, input_filename):
+def predict(x, model, tmpdir):
+
+    input_filename = os.path.join(tmpdir, "input.json")
+    output_filename = os.path.join(tmpdir, "output.json")
 
     torch_out = model(x)
     _, predicted = torch.max(torch_out, 1)
@@ -28,16 +30,16 @@ def write_input_file(x, model, input_filename):
         json.dump( data, f )
     
 
-    ezkl.forward(input_filename, "models/lenet.onnx", "output.json", run_args)
+    ezkl.forward(input_filename, "models/lenet.onnx", output_filename, run_args)
                 
-    with open("output.json", "r") as f:
+    with open(output_filename, "r") as f:
         scores = json.load(f)["output_data"]
     _, predicted_quantized = torch.max(torch.tensor(scores), 1)
 
     return predicted, predicted_quantized
 
 
-if __name__ == "__main__":
+def compute_accuracy(model, tmpdir):
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -47,29 +49,34 @@ if __name__ == "__main__":
     # Only works with batch size 1?
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
 
+    total = 0
+    correct = 0
+    correct_quantized = 0
+
+    for i, (images, labels) in enumerate(test_loader):
+        predicted, predicted_quantized = predict(images, model, tmpdir)
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        correct_quantized += (predicted_quantized == labels).sum().item()
+        
+        if (i + 1) % 1000 == 0:
+            print(f"Example {i + 1} of {len(test_dataset)}")
+
+    print(f"Measured accuracy on {total} samples")
+
+    accuracy = 100 * correct / total
+    accuracy_quantized = 100 * correct_quantized / total
+    print(f'Test Accuracy: {accuracy:.2f}% (original), {accuracy_quantized:.2f}% (quantized)')
+
+
+if __name__ == "__main__":
+
     with open("models/lenet.pickle", "rb") as f:
         model = pickle.load(f)
 
     with torch.no_grad():
         with tempfile.TemporaryDirectory() as tempdir:
-            input_filename = os.path.join(".", "input.json")
 
-            total = 0
-            correct = 0
-            correct_quantized = 0
-            for i, (images, labels) in enumerate(test_loader):
-                predicted, predicted_quantized = write_input_file(images, model, input_filename)
-
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-                correct_quantized += (predicted_quantized == labels).sum().item()
-                
-                if (i + 1) % 1000 == 0:
-                    print(f"Example {i + 1} of {len(test_dataset)}")
-
-            print(f"Measured accuracy on {total} samples")
-
-            accuracy = 100 * correct / total
-            accuracy_quantized = 100 * correct_quantized / total
-            print(f'Test Accuracy: {accuracy:.2f}% (original), {accuracy_quantized:.2f}% (quantized)')
+            compute_accuracy(model, tempdir)
                 
