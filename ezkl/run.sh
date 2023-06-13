d923116c142aa16c1b0da89eb4ed28c809e0a7c0
Georg Wiese <georgwiese@gmail.com>
2023-06-13 10:24:42 +0000

    Fix run script

--- ezkl/run.sh
+++ ezkl/run.sh
@@ -2,14 +2,24 @@
 
 set -e
 
-echo "=== Generating SRS"
-ezkl gen-srs --logrows 17 --params-path=ezkl/kzg.params
+# Fails for a smaller number of bits
+export BITS=18
+export LOGROWS=19
+
+echo "=== Running mock proof"
+ezkl mock -M models/lenet.onnx -D models/input.json --bits $BITS --logrows $LOGROWS
+
+if [ ! -f filename ]
+then
+    echo "=== Generating SRS"
+    ezkl gen-srs --params-path=ezkl/kzg_$LOGROWS.params --logrows $LOGROWS
+fi
 
 echo "=== Generating keys"
-ezkl setup -M models/lenet.onnx --params-path=ezkl/kzg.params --vk-path=ezkl/vk.key --pk-path=ezkl/pk.key --circuit-params-path=ezkl/circuit.params
+ezkl setup -M models/lenet.onnx --params-path=ezkl/kzg_$LOGROWS.params --vk-path=ezkl/vk.key --pk-path=ezkl/pk.key --circuit-params-path=ezkl/circuit.params --bits $BITS --logrows $LOGROWS
 
 echo "=== Generating proof"
-ezkl prove -M models/lenet.onnx -D models/input.json --pk-path=ezkl/pk.key --proof-path=ezkl/model.proof --params-path=ezkl/kzg.params --circuit-params-path=ezkl/circuit.params
+ezkl prove -M models/lenet.onnx -D models/input.json --pk-path=ezkl/pk.key --proof-path=ezkl/model.proof --params-path=ezkl/kzg_$LOGROWS.params --circuit-params-path=ezkl/circuit.params
 
 echo "=== Verifying proof"
-ezkl verify --proof-path=ezkl/model.proof --circuit-params-path=ezkl/circuit.params --vk-path=ezkl/vk.key --params-path=ezkl/kzg.params
+ezkl verify --proof-path=ezkl/model.proof --circuit-params-path=ezkl/circuit.params --vk-path=ezkl/vk.key --params-path=ezkl/kzg_$LOGROWS.params
