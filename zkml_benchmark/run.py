ba416e11d30db73617b0370ac947178ee2647aac
Georg Wiese <georgwiese@gmail.com>
2023-06-15 13:10:34 +0000

    Include forward pass

--- zkml_benchmark/run.py
+++ zkml_benchmark/run.py
@@ -4,6 +4,7 @@ import ezkl
 
 MODEL_PATH = "models/lenet.onnx"
 INPUTS_PATH = "models/input.json"
+INPUTS_AFTER_FORWARD_PATH = "models/input_after_forward.json"
 KZG_PARAMS_PATH = f"ezkl/kzg_{run_args.logrows}.params"
 CIRCUIT_PARAMS_PATH = "ezkl/circuit.params"
 PK_PATH = "ezkl/pk.key"
@@ -12,17 +13,22 @@ PROOF_PATH = "ezkl/lenet.proof"
 
 if __name__ == "__main__":
 
+    print("Running forward pass...")
+    ezkl.forward(INPUTS_PATH, MODEL_PATH, INPUTS_AFTER_FORWARD_PATH, run_args)
+
     print("Running mock...")
-    ezkl.mock(INPUTS_PATH, MODEL_PATH, run_args)
+    ezkl.mock(INPUTS_AFTER_FORWARD_PATH, MODEL_PATH, run_args)
 
     print("Generating KZG params...")
     ezkl.gen_srs(KZG_PARAMS_PATH, run_args.logrows)
 
     print("Generating keys...")
-    ezkl.setup(MODEL_PATH, VK_PATH, PK_PATH, KZG_PARAMS_PATH, CIRCUIT_PARAMS_PATH, run_args)
+    ezkl.setup(MODEL_PATH, VK_PATH, PK_PATH, KZG_PARAMS_PATH,
+               CIRCUIT_PARAMS_PATH, run_args)
 
     print("Generating proof...")
-    ezkl.prove(INPUTS_PATH, MODEL_PATH, PK_PATH, PROOF_PATH, KZG_PARAMS_PATH, "blake", "single", CIRCUIT_PARAMS_PATH)
+    ezkl.prove(INPUTS_AFTER_FORWARD_PATH, MODEL_PATH, PK_PATH, PROOF_PATH,
+               KZG_PARAMS_PATH, "blake", "single", CIRCUIT_PARAMS_PATH)
 
     print("Verifying proof...")
     ezkl.verify(PROOF_PATH, CIRCUIT_PARAMS_PATH, VK_PATH, KZG_PARAMS_PATH)
--- zkml_benchmark/run_args.py
+++ zkml_benchmark/run_args.py
@@ -2,26 +2,7 @@ import ezkl
 
 # Output of:
 # ezkl calibrate-settings -M models/lenet.onnx -D models/input.json --target resources
-# But fails with:
-# error: constraint not satisfied
-
-#   Cell layout in region 'model':
-#     | Offset | A1 | A2 |
-#     +--------+----+----+
-#     | 295518 | x0 | x1 | <--{ Gate 'RANGE' applied here
-
-#   Constraint '':
-#     ((S6 * (0x2 - S6)) * (0x3 - S6)) * (x0 - x1) = 0
-
-#   Assigned cell values:
-#     x0 = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593effe0744
-#     x1 = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593effdffe6
 run_args = ezkl.PyRunArgs()
-# run_args.scale = 5
-# run_args.bits = 19
-# run_args.logrows = 20
-
-# Works on first test example
-run_args.scale = 7
-run_args.bits = 23
-run_args.logrows = 24
+run_args.scale = 5
+run_args.bits = 19
+run_args.logrows = 20
